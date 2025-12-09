ac94738e8ed611b432d57c8b5c0531c76b8a296f
Wadim Seminsky <w.v.seminsky@gmail.com>
2025-12-09 22:58:32 +0000

    Console CompileError
    WebAssembly.instantiate(): unknown type form: 2 @

--- next.config.ts
+++ next.config.ts
@@ -31,10 +31,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true, topLevelAwait: true };
-    return config;
-  },
 };
 
 export default nextConfig;
