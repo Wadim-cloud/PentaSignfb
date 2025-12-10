5947f17316e57d82259f714d49f97d981ac2bc3d
Wadim Seminsky <w.v.seminsky@gmail.com>
2025-12-10 00:23:59 +0000

    https://github.com/penpot/penpot-plugins-samples/tree/main/theme here is

--- public/penpot-plugin/code.js
+++ public/penpot-plugin/code.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{penpot.ui.open("PentaSign","/",{width:800,height:600});})();
+"use strict";(()=>{penpot.ui.open("PentaSign","/",{width:800,height:600,permissions:["clipboard-write"]});})();
